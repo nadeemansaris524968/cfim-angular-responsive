70a4ef7d5644614e500563049c739aa76a4e4ad3
Nadeem Ansari <nadeem_ansari64@outlook.com>
2025-05-01 18:36:50 -0500

    add footer and responsiveness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+
+const appRoutes: Routes = [
+  { path: '', component: MainComponent, pathMatch: 'full' },
+];
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, HeroComponent, MainComponent, FooterComponent],
   imports: [BrowserModule],
   bootstrap: [AppComponent],
 })
