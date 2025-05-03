d7d21cf563e1f0bc2f27b93b2643b70424237ada
Nadeem Ansari <nadeem_ansari64@outlook.com>
2025-05-03 14:45:27 -0500

    close mobiel-nav on outside click

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: false,
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css',
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
   isToggled = false;
 
+  constructor(private elRef: ElementRef) {}
+
   toggleHamburger() {
     this.isToggled = !this.isToggled;
+  }
+
+  @HostListener('document:click', ['$event'])
+  @HostListener('document:touchstart', ['$event'])
+  handleClickOutside(event: Event) {
+    if (!this.elRef.nativeElement.contains(event.target) && this.isToggled) {
+      this.toggleHamburger();
+    }
   }
 }
