5fefe2e4c8327b892c3674d4101115ba47a67450
Nadeem Ansari <nadeem_ansari64@outlook.com>
2025-05-03 19:38:12 -0500

    update routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CardsComponent } from './cards/cards.component';
+import { CredentialsComponent } from './credentials/credentials.component';
+import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
-import { MainComponent } from './main/main.component';
-import { FooterComponent } from './footer/footer.component';
-import { CardsComponent } from './cards/cards.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { MissionComponent } from './mission/mission.component';
-import { QocComponent } from './qoc/qoc.component';
-import { ServicesOfferedComponent } from './services-offered/services-offered.component';
-import { CredentialsComponent } from './credentials/credentials.component';
 import { InsuranceComponent } from './insurance/insurance.component';
 import { LocationComponent } from './location/location.component';
-import { ServiceDetailComponent } from './service-detail/service-detail.component';
+import { MainComponent } from './main/main.component';
+import { MissionComponent } from './mission/mission.component';
+import { QocComponent } from './qoc/qoc.component';
 import { SeenTodayComponent } from './seen-today/seen-today.component';
+import { ServicesOfferedComponent } from './services-offered/services-offered.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 const appRoutes: Routes = [
   { path: '', component: MainComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HeroComponent, MainComponent, FooterComponent, CardsComponent, WelcomeComponent, MissionComponent, QocComponent, ServicesOfferedComponent, CredentialsComponent, InsuranceComponent, LocationComponent, ServiceDetailComponent, SeenTodayComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    MainComponent,
+    FooterComponent,
+    CardsComponent,
+    WelcomeComponent,
+    MissionComponent,
+    QocComponent,
+    ServicesOfferedComponent,
+    CredentialsComponent,
+    InsuranceComponent,
+    LocationComponent,
+    SeenTodayComponent,
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/service-detail/service-detail.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-service-detail',
-  standalone: false,
-  templateUrl: './service-detail.component.html',
-  styleUrl: './service-detail.component.css',
-})
-export class ServiceDetailComponent {}
